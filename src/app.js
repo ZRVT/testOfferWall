9972940d09d0b2001ff3868ad0ae94150ace408e
ZRVT <144631997+ZRVT@users.noreply.github.com>
2025-12-31 19:44:52 +0800

    added new page and made campaigns page the general page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import CampaignsPage from './pages/CampaignPage'
+import CampaignDetailsPage from './pages/CampaignDetailsPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CampaignsPage />} />
+      <Route path="/campaigns/:campaignId" element={<CampaignDetailsPage />} />
     </Routes>
   )
 }
---
+++ src/pages/CampaignDetailsPage.js
@@ -0,0 +1,12 @@
+import CampaignView from '../components/campaign'
+
+const CampaignsPage = () => {
+  return (
+    <>
+      <h1>Campaigns</h1>
+      <CampaignView />
+    </>
+  )
+}
+
+export default CampaignsPage
